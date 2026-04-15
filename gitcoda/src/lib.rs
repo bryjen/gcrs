f092fd3637c5e248b0a6270076c321f770abe5bc
bry <bryancarlomiguel@gmail.com>
2026-04-15 14:24:19 +0000

    codex can go fucking kill itself

--- gitcoda/src/lib.rs
+++ gitcoda/src/lib.rs
@@ -1,12 +1,2 @@
-pub mod app;
-pub mod components;
 pub mod models;
-pub mod pages;
-
-#[cfg(feature = "hydrate")]
-#[wasm_bindgen::prelude::wasm_bindgen]
-pub fn hydrate() {
-    _ = console_log::init_with_level(log::Level::Debug);
-    console_error_panic_hook::set_once();
-    leptos::mount::hydrate_islands();
-}
+pub mod services;
